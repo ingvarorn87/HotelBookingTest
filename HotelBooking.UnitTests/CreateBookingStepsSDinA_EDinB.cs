17545b25ee03a539a9081e0ba0282980035141a8
kivulallo <kristof@kristof.wtf>
2019-11-05 22:37:15 +0100

    generated specflow skeletons, needs logic

---
+++ HotelBooking.UnitTests/CreateBookingStepsSDinA_EDinB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace HotelBooking.UnitTests
+{
+    [Binding]
+    public class CreateBookingStepsSDinA_EDinB
+    {
+        [Given(@"I have entered ([0-9]*),([0-9]*),([0-9]*) as start date")]
+        public void GivenIHaveEnteredAsStartDate(int year, int month, int day)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have entered ([0-9]*),([0-9]*),([0-9]*) as end date")]
+        public void GivenIHaveEnteredAsEndDate(int year, int month, int day)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press book")]
+        public void WhenIPressBook()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The result should be false")]
+        public void ThenTheResultShouldBeFalse()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ HotelBooking.UnitTests/CreateBookingStepsSDinB_EDinB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace HotelBooking.UnitTests
+{
+    [Binding]
+    public class CreateBookingStepsSDinB_EDinB
+    {
+        [Given(@"I have entered ([0-9]*),([0-9]*),([0-9]*) as start date")]
+        public void GivenIHaveEnteredAsStartDate(int year, int month, int day)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have entered ([0-9]*),([0-9]*),([0-9]*) as end date")]
+        public void GivenIHaveEnteredAsEndDate(int year, int month, int day)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press book")]
+        public void WhenIPressBook()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The booking should be approved")]
+        public void ThenTheBookingShouldBeApproved()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
