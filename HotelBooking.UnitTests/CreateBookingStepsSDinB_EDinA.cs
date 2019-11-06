ebfdec6084e92446faddd7d4c7a06b75e948ec04
kivulallo <kristof@kristof.wtf>
2019-11-06 15:49:26 +0100

    sometimes it works, sometimes not.

--- HotelBooking.UnitTests/CreateBookingStepsSDinB_EDinA.cs
+++ HotelBooking.UnitTests/CreateBookingStepsSDinB_EDinA.cs
@@ -1,33 +1,51 @@
 ﻿using System;
+using HotelBooking.Core;
+using HotelBooking.UnitTests.Fakes;
 using TechTalk.SpecFlow;
+using Xunit;
 
 namespace HotelBooking.UnitTests
 {
     [Binding]
+    [Scope(Feature = "Create booking", Tag = "SDinB_EDinA")]
     public class CreateBookingStepsSDinB_EDinA
     {
-        [Given(@"I have entered ([0-9]*),([0-9]*),([0-9]*) as start date")]
-        public void GivenIHaveEnteredAsStartDate(int year, int month, int day)
+        private Booking booking = new Booking();
+        private bool result;
+        static DateTime start = new DateTime(2019, 12, 9);
+        static DateTime end = new DateTime(2019, 12, 15);
+        static IRepository<Booking> bookingRepository = new FakeBookingRepository(start, end);
+        static IRepository<Room> roomRepository = new FakeRoomRepository();
+        private BookingManager bookingManager = new BookingManager(bookingRepository, roomRepository);
+
+        [Given(@"I have entered (.*),(.*),(.*) as start date")]
+        public void GivenIHaveEnteredAsStartDate(string p0, string p1, string p2)
         {
-            ScenarioContext.Current.Pending();
+            Int32.TryParse(p0, out int year);
+            Int32.TryParse(p1, out int month);
+            Int32.TryParse(p2, out int day);
+            booking.StartDate = new DateTime(year, month, day);
         }
         
-        [Given(@"I have entered ([0-9]*),([0-9]*),([0-9]*) as end date")]
-        public void GivenIHaveEnteredAsEndDate(int year, int month, int day)
+        [Given(@"I have entered (.*),(.*),(.*) as end date")]
+        public void GivenIHaveEnteredAsEndDate(string p0, string p1, string p2)
         {
-            ScenarioContext.Current.Pending();
+            Int32.TryParse(p0, out int year);
+            Int32.TryParse(p1, out int month);
+            Int32.TryParse(p2, out int day);
+            booking.EndDate = new DateTime(year, month, day);
         }
         
         [When(@"I press book")]
         public void WhenIPressBook()
         {
-            ScenarioContext.Current.Pending();
+            result = bookingManager.CreateBooking(booking);
         }
-        
+
         [Then(@"the result should be false")]
         public void ThenTheResultShouldBeFalse()
         {
-            ScenarioContext.Current.Pending();
+            Assert.False(result);
         }
     }
 }
--- HotelBooking.UnitTests/CreateBookingStepsSDinB_EDinB.cs
+++ HotelBooking.UnitTests/CreateBookingStepsSDinB_EDinB.cs
@@ -1,33 +1,52 @@
-﻿using System;
+﻿using HotelBooking.Core;
+using Xunit;
+using System;
+using HotelBooking.UnitTests.Fakes;
+
 using TechTalk.SpecFlow;
 
 namespace HotelBooking.UnitTests
 {
     [Binding]
+    [Scope(Feature = "Create booking", Tag = "SDinB_EDinB")]
     public class CreateBookingStepsSDinB_EDinB
     {
-        [Given(@"I have entered ([0-9]*),([0-9]*),([0-9]*) as start date")]
-        public void GivenIHaveEnteredAsStartDate(int year, int month, int day)
+        private Booking booking = new Booking();
+        private bool result;
+        static DateTime start = new DateTime(2019, 12, 9);
+        static DateTime end = new DateTime(2019, 12, 15);
+        static IRepository<Booking> bookingRepository = new FakeBookingRepository(start, end);
+        static IRepository<Room> roomRepository = new FakeRoomRepository();
+        private BookingManager bookingManager = new BookingManager(bookingRepository, roomRepository);
+
+        [Given(@"I have entered (.*),(.*),(.*) as start date")]
+        public void GivenIHaveEnteredAsStartDate(string p0, string p1, string p2)
         {
-            ScenarioContext.Current.Pending();
+            Int32.TryParse(p0, out int year);
+            Int32.TryParse(p1, out int month);
+            Int32.TryParse(p2, out int day);
+            booking.StartDate = new DateTime(year, month, day);
         }
         
-        [Given(@"I have entered ([0-9]*),([0-9]*),([0-9]*) as end date")]
-        public void GivenIHaveEnteredAsEndDate(int year, int month, int day)
+        [Given(@"I have entered (.*),(.*),(.*) as end date")]
+        public void GivenIHaveEnteredAsEndDate(string p0, string p1, string p2)
         {
-            ScenarioContext.Current.Pending();
+            Int32.TryParse(p0, out int year);
+            Int32.TryParse(p1, out int month);
+            Int32.TryParse(p2, out int day);
+            booking.EndDate = new DateTime(year, month, day);
         }
         
         [When(@"I press book")]
         public void WhenIPressBook()
         {
-            ScenarioContext.Current.Pending();
+            result = bookingManager.CreateBooking(booking);
         }
         
         [Then(@"The booking should be approved")]
         public void ThenTheBookingShouldBeApproved()
         {
-            ScenarioContext.Current.Pending();
+            Assert.True(result);
         }
     }
 }
